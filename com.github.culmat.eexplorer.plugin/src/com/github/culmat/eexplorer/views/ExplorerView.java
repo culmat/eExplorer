6c39c97e23b76c71fbac84edb65be697b60b9448
void_000 <void_000@virt-win8>
2014-04-17 07:42:05 -0700

    initial

---
+++ com.github.culmat.eexplorer.plugin/src/com/github/culmat/eexplorer/views/ExplorerView.java
@@ -0,0 +1,84 @@
+package com.github.culmat.eexplorer.views;
+
+import java.io.File;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTError;
+import org.eclipse.swt.ole.win32.OLE;
+import org.eclipse.swt.ole.win32.OleAutomation;
+import org.eclipse.swt.ole.win32.OleClientSite;
+import org.eclipse.swt.ole.win32.OleFrame;
+import org.eclipse.swt.ole.win32.Variant;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IViewSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.ViewPart;
+
+import com.github.culmat.eexplorer.views.SyncWithPackageManagerListener.FileSelectionListener;
+
+public class ExplorerView extends ViewPart implements FileSelectionListener {
+
+	/**
+	 * The ID of the view as specified by the extension.
+	 */
+	public static final String ID = "com.github.culmat.eexplorer.views.ExplorerView";
+
+	private OleClientSite site;
+
+	private SyncWithPackageManagerListener selectionListener;
+
+	private OleAutomation auto;
+	static final int Navigate = 0x68;
+
+	@Override
+	public void init(IViewSite site) throws PartInitException {
+		super.init(site);
+		selectionListener = new SyncWithPackageManagerListener(site.getWorkbenchWindow(), this);
+		Display.getDefault().asyncExec(new Runnable()
+		{
+		  @Override
+		  public void run()
+		  {
+			  selectionListener.setEnabled(true);
+		  }
+		});
+	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();
+		selectionListener.setEnabled(false);
+	}
+	
+
+	@Override
+	public void createPartControl(Composite parent) {
+		try {
+			OleFrame frame = new OleFrame(parent, SWT.NONE);
+			site = new OleClientSite(frame, SWT.NONE, "Shell.Explorer.1");
+			site.doVerb(OLE.OLEIVERB_INPLACEACTIVATE);
+			auto = new OleAutomation(site);
+			auto.invoke(Navigate, new Variant[] { new Variant("c:\\") });
+		} catch (SWTError e) {
+			System.out.println("Unable to open activeX control");
+			return;
+		}
+
+	}
+
+	@Override
+	public void setFocus() {
+		site.setFocus();
+	}
+
+	@Override
+	public void select(File selection) {
+		System.out.println(selection.getAbsolutePath());
+		try {
+			auto.invoke(Navigate, new Variant[] { new Variant(selection.getCanonicalPath()) });
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ com.github.culmat.eexplorer.plugin/src/com/github/culmat/eexplorer/views/SyncWithPackageManagerListener.java
@@ -0,0 +1,85 @@
+package com.github.culmat.eexplorer.views;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.ISelectionService;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+
+public class SyncWithPackageManagerListener implements ISelectionListener {
+	private static final String PACKAGE_EXPLORER_ID = "org.eclipse.jdt.ui.PackageExplorer";
+	private boolean enabled;
+	private final ISelectionService selectionService;
+	private final FileSelectionListener listener;
+	private File lastSelection;
+
+
+	public static interface FileSelectionListener {
+		void select(File selection);
+	}
+	
+	SyncWithPackageManagerListener(IWorkbenchWindow workbenchWindow, FileSelectionListener listener) {
+		this.listener = listener;
+		selectionService = workbenchWindow.getSelectionService();
+	}
+
+	@Override
+	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+		if (!enabled)
+			return;
+		IStructuredSelection sel = (IStructuredSelection) selection;
+		if (sel == null)
+			return;
+		@SuppressWarnings("rawtypes")
+		Iterator iterator = sel.iterator();
+		while (iterator.hasNext()) {
+			Object item = iterator.next();
+			if (item instanceof IAdaptable) {
+				IResource resource = (IResource) ((IAdaptable) item)
+						.getAdapter(IResource.class);
+				if (resource != null) {
+					IPath location = resource.getLocation();
+					if(location != null){
+						File file = location.toFile();
+						if(!file.isDirectory()) file = file.getParentFile();
+						notifyListener(file);
+					}
+				}
+				
+				
+				
+
+			}
+		}
+	}
+
+	private void notifyListener(File file) {
+		if(file.equals(lastSelection)) return;
+		lastSelection = file;
+		listener.select(file);
+	}
+
+	public void setEnabled(boolean enabled) {
+		if (enabled == this.enabled)
+			return;
+		this.enabled = enabled;
+		if (enabled) {
+			selectionService
+					.addPostSelectionListener(PACKAGE_EXPLORER_ID, this);
+			ISelection selection = selectionService
+					.getSelection(PACKAGE_EXPLORER_ID);
+			selectionChanged(null, selection);
+		} else {
+			selectionService.removePostSelectionListener(PACKAGE_EXPLORER_ID,
+					this);
+		}
+	}
+}
